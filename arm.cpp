bb8776ae706d8cedaf31dcb1785d244f916d58ea
NotoriousR45 <91585770+NotoriousR45@users.noreply.github.com>
2021-09-29 10:23:45 +0530

    Add files via upload

---
+++ arm.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int n,r,temp,sum=0;
+cout<<"enter a number";
+cin>>n;
+temp=n;
+while(n>0)
+{
+r=n%10;
+sum=sum+(r*r*r);
+n=n/10;
+}
+if(temp==sum)
+cout<<"armstrong number"<<endl;
+else
+cout<<"not a armstrong number"<<endl;
+return 0;
+} 
